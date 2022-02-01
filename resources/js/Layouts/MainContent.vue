a67cd27a18fb1f9f965c90c91edd70fc4fdff013
Paulo Dantas <paulodantas19@gmail.com>
2022-01-31 21:42:34 -0300

    router, categories model and controller, posts model and controller and routing

---
+++ resources/js/Layouts/MainContent.vue
@@ -0,0 +1,41 @@
+<template>
+        <main class="w-screen space-y-6 flex-1 flex flex-col w-10/12">
+            <slot name="header"></slot>
+            <slot name="dashboard_content"></slot>
+        </main>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+import JetDropdown from "@/Jetstream/Dropdown";
+import JetDropdownLink from "@/Jetstream/DropdownLink";
+
+export default defineComponent({
+    props: {
+        title: String,
+    },
+    components: {
+        JetDropdown,
+        JetDropdownLink,
+    },
+    data() {
+        return {
+            showingNavigationDropdown: true,
+        }
+    },
+    methods: {
+        switchToTeam(team) {
+            this.$inertia.put(route('current-team.update'), {
+                'team_id': team.id
+            }, {
+                preserveState: false
+            })
+        },
+
+        logout() {
+            this.$inertia.post(route('logout'));
+        },
+    }
+})
+</script>
